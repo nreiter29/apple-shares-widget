fd0d7a353c4d89e6742c326dc2e6c796af73431f
Noah Reiter <n.reiter@alpin11.com>
2022-05-10 07:59:13 +0200

    finsihed Apple shares

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Aktie from './aktien';
+// import CanvasJSReact from './canvasjs.react';
+// import { render } from '@testing-library/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		return (
+      <body>
+        <div className='iConBox'>
+        <div className="box">
+            <Aktie url="https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey=cMJpjZ_3DNh4bOxSPpvOwIln0gfewRfV"/>
+        </div>
+        </div>
+      </body>
+
+    ); 
 }
 
 export default App;
+// https://api.polygon.io/v2/aggs/ticker/SMCI/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey=cMJpjZ_3DNh4bOxSPpvOwIln0gfewRfV
