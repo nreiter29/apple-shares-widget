c4d1891b432ed10f5891e5c4e4efc8207a59984d
Noah Reiter <n.reiter@alpin11.com>
2022-05-10 08:02:31 +0200

    added signature

--- src/aktien.js
+++ src/aktien.js
@@ -42,5 +42,13 @@ export default function Aktie(props) {
   </div>);
 }
 
+//          ______
+// |\      |       \
+// | \     |        |
+// |  \    |______ /
+// |   \   |       \
+// |    \  |        \
+// |     \ |         \
+// |      \|          \
 
 // https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey=cMJpjZ_3DNh4bOxSPpvOwIln0gfewRfV
